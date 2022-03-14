ca2d644e7158044b31d4e60a773d70cc7ea57a8a
Mocktar ISSA <mocktarissa@gmail.com>
2022-03-14 11:22:41 +0100

    resolve parsing issue

--- src/container.ts
+++ src/container.ts
@@ -2,20 +2,8 @@ import { spawn, exec } from "child_process";
 import { lstat } from "fs";
 export class DockerContainer {
   // id: string;
-  parseData(data: any): any {}
   static list(): Promise<any[]> {
-    const cmd = spawn("docker", [
-      "ps",
-      "--format",
-      "{'Id':'{{.ID}}','Name': '{{.Names}}','Images': '{{.Images}}'},",
-    ]);
-
-    const cmdAdditional = spawn("docker", [
-      "ps",
-      "--format",
-      "--filter ID=da43e8531b6d",
-      "{{json .}}",
-    ]);
+    const cmd = spawn("docker", ["ps", "--format", "{{json .}}"]);
     return new Promise<any[]>((resolve, reject) => {
       let containers = "";
       cmd.stdout.on("data", (chunk: any) => {
