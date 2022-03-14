c48a3ae8abc3586ba0bc76a635d301a5dad6013a
Mocktar ISSA <mocktarissa@gmail.com>
2022-03-14 12:14:37 +0100

    Chore refactorisation de index

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,3 @@
-// import { spawn } from "child_process";
-// const ls = spawn('docker', ['ps', '--format', "{{json .}}"]);
-
-// let i = 0;
-
-// ls.stdout.on('data', (data) => {
-//   let obj = JSON.parse(data);
-//   console.log(i,obj);
-//   i++;
-// });
-
-// ls.stderr.on('data', (data) => {
-//   let obj = JSON.parse(data);
-//   console.log(obj);
-//   i++;
-
-// });
-
-// ls.on('exit', (code) => {
-//   console.log(`Child exited with code ${code}`);
-// });
-
-// ls.on('error', error => {
-//   console.log(error);
-// })
-
 import { DockerContainer } from "./container";
 
 DockerContainer.list()
